732b23c1ab2af150e0e6d73afd6373b06c3b9ef5
Yaroslav Korotenko <Kor1k13002@gmail.com>
2025-09-19 00:03:02 +0300

    Implement cache decorator for Mate Academy

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
+from functools import wraps
 from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_store = {}
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> object:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key in cache_store:
+            print("Getting from cache")
+            return cache_store[key]
+        print("Calculating new result")
+        result = func(*args, **kwargs)
+        cache_store[key] = result
+        return result
+
+    return wrapper
